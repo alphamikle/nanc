a92c46184863107a3fd5ec603363acf8b60e152d
alphamikle <xelioys@gmail.com>
2022-12-29 15:59:48 -0300

    fix admin build process

--- nanc_backend/ci.sh
+++ nanc_backend/ci.sh
@@ -1,4 +1,8 @@
-# flutter build web
+flutter build web \
+--dart-define=SUPA_HOST="$NANC_SUPA_HOST" \
+--dart-define=SUPA_KEY="$NANC_SUPA_KEY" \
+--dart-define="$NANC_SECRET_KEY"="$NANC_SECRET_VALUE"
+
 cp -R ./build/web/ "$NANC_ADMIN_REPO"
 cd "$NANC_ADMIN_REPO"
 git add .
