9b6642974e96137110daaedb478825af5f7cee06
alphamikle <xelioys@gmail.com>
2023-01-01 23:08:33 -0300

    cliprrect docs

--- rich_renderer/lib/src/renderers/center/center_renderer.dart
+++ rich_renderer/lib/src/renderers/center/center_renderer.dart
@@ -4,8 +4,6 @@ import 'package:markdown/markdown.dart' as md;
 import 'package:rich_renderer/rich_renderer.dart';
 import 'package:rich_renderer/src/renderers/center/center_arguments.dart';
 import 'package:rich_renderer/src/renderers/property/mapper/properties_extractor.dart';
-import 'package:rich_renderer/src/rich_renderer.dart';
-import 'package:rich_renderer/src/tag_renderer.dart';
 import 'package:rich_renderer/src/tools/widgets_compactor.dart';
 
 const String description = '''
@@ -13,15 +11,15 @@ const String description = '''
 
 A widget that centers its child within itself.
 
-This widget will be as big as possible if its dimensions are constrained and [widthFactor](https://api.flutter.dev/flutter/widgets/Align/widthFactor.html) and [heightFactor](https://api.flutter.dev/flutter/widgets/Align/heightFactor.html) are null. If a dimension is unconstrained and the corresponding size factor is null then the widget will match its child's size in that dimension. If a size factor is non-null then the corresponding dimension of this widget will be the product of the child's dimension and the size factor. For example if widthFactor is 2.0 then the width of this widget will always be twice its child's width.
+This widget will be as big as possible if its dimensions are constrained and [widthFactor](widgets/Align/widthFactor.html) and [heightFactor](widgets/Align/heightFactor.html) are null. If a dimension is unconstrained and the corresponding size factor is null then the widget will match its child's size in that dimension. If a size factor is non-null then the corresponding dimension of this widget will be the product of the child's dimension and the size factor. For example if widthFactor is 2.0 then the width of this widget will always be twice its child's width.
 
 See also:
 
-- [Align](https://api.flutter.dev/flutter/widgets/Align-class.html), which lets you arbitrarily position a child within itself, rather than just centering it.
-- [Row](https://api.flutter.dev/flutter/widgets/Row-class.html), a widget that displays its children in a horizontal array.
-- [Column](https://api.flutter.dev/flutter/widgets/Column-class.html), a widget that displays its children in a vertical array.
-- [Container](https://api.flutter.dev/flutter/widgets/Container-class.html), a convenience widget that combines common painting, positioning, and sizing widgets.
-- The catalog of [layout widgets](https://flutter.dev/widgets/layout/).
+*   [Align](widgets/Align-class.html), which lets you arbitrarily position a child within itself, rather than just centering it.
+*   [Row](widgets/Row-class.html), a widget that displays its children in a horizontal array.
+*   [Column](widgets/Column-class.html), a widget that displays its children in a vertical array.
+*   [Container](widgets/Container-class.html), a convenience widget that combines common painting, positioning, and sizing widgets.
+*   The [catalog of layout widgets](https://flutter.dev/widgets/layout/).
 ''';
 
 TagRenderer centerRenderer() {
--- rich_renderer/lib/src/renderers/clipr_rect/clipr_rect_renderer.dart
+++ rich_renderer/lib/src/renderers/clipr_rect/clipr_rect_renderer.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:icons/icons.dart';
 import 'package:markdown/markdown.dart' as md;
+import 'package:rich_renderer/rich_renderer.dart';
 import 'package:rich_renderer/src/renderers/clipr_rect/clipr_rect_arguments.dart';
 import 'package:rich_renderer/src/renderers/property/mapper/properties_extractor.dart';
-import 'package:rich_renderer/src/rich_renderer.dart';
-import 'package:rich_renderer/src/tag_renderer.dart';
 import 'package:rich_renderer/src/tools/widgets_compactor.dart';
+
+const String description = '''
+A widget that clips its child using a rounded rectangle.
+
+By default, [ClipRRect](widgets/ClipRRect-class.html) uses its own bounds as the base rectangle for the clip, but the size and location of the clip can be customized using a custom [clipper](widgets/ClipRRect/clipper.html).
+
+This example shows various [ClipRRect](widgets/ClipRRect-class.html)s applied to containers.
+
+[_link_](# "Copy link to clipboard")
+
+To create a local project with this code sample, run:  
+flutter create --sample=widgets.ClipRRect.1 mysample
+
+Troubleshooting
+---------------
+
+### Why doesn't my [ClipRRect](widgets/ClipRRect-class.html) child have rounded corners?
+
+When a [ClipRRect](widgets/ClipRRect-class.html) is bigger than the child it contains, its rounded corners could be drawn in unexpected positions. Make sure that [ClipRRect](widgets/ClipRRect-class.html) and its child have the same bounds (by shrinking the [ClipRRect](widgets/ClipRRect-class.html) with a [FittedBox](widgets/FittedBox-class.html) or by growing the child).
+
+This example shows a [ClipRRect](widgets/ClipRRect-class.html) that adds round corners to an image.
+
+[_link_](# "Copy link to clipboard")
+
+To create a local project with this code sample, run:  
+flutter create --sample=widgets.ClipRRect.2 mysample
+
+See also:
+
+*   [CustomClipper](rendering/CustomClipper-class.html), for information about creating custom clips.
+*   [ClipRect](widgets/ClipRect-class.html), for more efficient clips without rounded corners.
+*   [ClipOval](widgets/ClipOval-class.html), for an elliptical clip.
+*   [ClipPath](widgets/ClipPath-class.html), for an arbitrarily shaped clip.
+''';
 
 TagRenderer clipRRectRenderer() {
   return TagRenderer(
@@ -13,8 +46,27 @@ TagRenderer clipRRectRenderer() {
     tag: 'clipRRect',
     pattern: RegExp(r'<clipRRect.*>'),
     endPattern: RegExp('</clipRRect>'),
+    description: const TagDescription(
+      description: description,
+      arguments: [
+        TagArgument('topLeft', {'double', 'null'}),
+        TagArgument('topRight', {'double', 'null'}),
+        TagArgument('bottomRight', {'double', 'null'}),
+        TagArgument('bottomRight', {'double', 'null'}),
+        TagArgument('bottomLeft', {'double', 'null'}),
+        TagArgument('all', {'double', 'null'}),
+      ],
+      properties: [],
+    ),
     example: '''
-TODO
+<container width="300" height="300" color="#457FDA">
+  <center>
+    <clipRRect topLeft="64" bottomRight="64">
+      <container width="100" height="100" color="#7BDA45">
+      </container>
+    </clipRRect>
+  </center>
+</container>
 ''',
     builder: (BuildContext context, md.Element element, RichRenderer richRenderer) async {
       final ClipRRectArguments arguments = ClipRRectArguments.fromJson(element.attributes);
