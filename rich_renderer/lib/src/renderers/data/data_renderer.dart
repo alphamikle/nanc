91f8ddbceb7aad2d5cb45a25d7a7ed99bc15f112
alphamikle <xelioys@gmail.com>
2023-01-06 14:26:49 -0300

    data documentation

--- rich_renderer/lib/src/renderers/data/data_renderer.dart
+++ rich_renderer/lib/src/renderers/data/data_renderer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:icons/icons.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:rich_renderer/rich_renderer.dart';
+import 'package:rich_renderer/src/documentation/arguments/common.dart';
 import 'package:rich_renderer/src/renderers/data/data_arguments.dart';
 
 TagRenderer dataRenderer() {
@@ -10,8 +11,41 @@ TagRenderer dataRenderer() {
     tag: 'data',
     pattern: RegExp(r'<data.*/>'),
     endPattern: null,
+    description: TagDescription(
+      description: '''
+# Data
+
+Data - is a special tag, which allows you to store...the data! Who would think that? It may be helpful, for example, if you have some reusable param, which need in many places of your UI.
+
+Data consumes one required argument "id" and any another arguments, which you want. See example code for more info.
+''',
+      arguments: [
+        idArg(),
+        anyArg(),
+      ],
+      properties: [],
+    ),
     example: '''
-TODO
+<data id="gap" left="16" top="12" right="16" bottom="32"/>
+
+<container width="300" height="500" color="#457FDA">
+  <prop:padding left="{{ data(gap).left }}" top="{{ data(gap).top }}" right="{{ data(gap).right }}" bottom="{{ data(gap).bottom }}"/>
+  <container color="#7BDA45">
+    <prop:padding left="{{ data(gap).left }}" top="{{ data(gap).top }}" right="{{ data(gap).right }}" bottom="{{ data(gap).bottom }}"/>
+    <container color="#457FDA">
+      <prop:padding left="{{ data(gap).left }}" top="{{ data(gap).top }}" right="{{ data(gap).right }}" bottom="{{ data(gap).bottom }}"/>
+      <container color="#7BDA45">
+        <prop:padding left="{{ data(gap).left }}" top="{{ data(gap).top }}" right="{{ data(gap).right }}" bottom="{{ data(gap).bottom }}"/>
+        <container color="#457FDA">
+          <prop:padding left="{{ data(gap).left }}" top="{{ data(gap).top }}" right="{{ data(gap).right }}" bottom="{{ data(gap).bottom }}"/>
+          <container color="#7BDA45">
+            <prop:padding left="{{ data(gap).left }}" top="{{ data(gap).top }}" right="{{ data(gap).right }}" bottom="{{ data(gap).bottom }}"/>
+          </container>
+        </container>
+      </container>
+    </container>
+  </container>
+</container>
 ''',
     builder: (BuildContext context, md.Element element, RichRenderer richRenderer) async {
       final DataArguments arguments = DataArguments.fromJson(element.attributes);
