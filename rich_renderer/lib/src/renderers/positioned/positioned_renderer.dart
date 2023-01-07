8eb0468b8f526d331a36ece12347c63ad5976505
alphamikle <xelioys@gmail.com>
2023-01-07 14:17:30 -0300

    positioned documentation

--- rich_renderer/lib/src/renderers/positioned/positioned_renderer.dart
+++ rich_renderer/lib/src/renderers/positioned/positioned_renderer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:icons/icons.dart';
 import 'package:markdown/markdown.dart' as md;
+import 'package:rich_renderer/rich_renderer.dart';
+import 'package:rich_renderer/src/documentation/arguments/padding_arguments.dart';
+import 'package:rich_renderer/src/documentation/arguments/size_arguments.dart';
 import 'package:rich_renderer/src/renderers/positioned/positioned_arguments.dart';
 import 'package:rich_renderer/src/renderers/property/mapper/properties_extractor.dart';
 import 'package:rich_renderer/src/rich_renderer.dart';
@@ -13,8 +16,51 @@ TagRenderer positionedRenderer() {
     tag: 'positioned',
     pattern: RegExp(r'<positioned.*>'),
     endPattern: RegExp('</positioned>'),
+    description: TagDescription(
+      description: '''
+# [Positioned]()
+
+A widget that controls where a child of a [Stack](widgets/Stack-class.html) is positioned.
+
+A [Positioned](widgets/Positioned-class.html) widget must be a descendant of a [Stack](widgets/Stack-class.html), and the path from the [Positioned](widgets/Positioned-class.html) widget to its enclosing [Stack](widgets/Stack-class.html) must contain only [StatelessWidget](widgets/StatelessWidget-class.html)s or [StatefulWidget](widgets/StatefulWidget-class.html)s (not other kinds of widgets, like [RenderObjectWidget](widgets/RenderObjectWidget-class.html)s).
+
+If a widget is wrapped in a [Positioned](widgets/Positioned-class.html), then it is a _positioned_ widget in its [Stack](widgets/Stack-class.html). If the [top](widgets/Positioned/top.html) property is non-null, the top edge of this child will be positioned [top](widgets/Positioned/top.html) layout units from the top of the stack widget. The [right](widgets/Positioned/right.html), [bottom](widgets/Positioned/bottom.html), and [left](widgets/Positioned/left.html) properties work analogously.
+
+If both the [top](widgets/Positioned/top.html) and [bottom](widgets/Positioned/bottom.html) properties are non-null, then the child will be forced to have exactly the height required to satisfy both constraints. Similarly, setting the [right](widgets/Positioned/right.html) and [left](widgets/Positioned/left.html) properties to non-null values will force the child to have a particular width. Alternatively the [width](widgets/Positioned/width.html) and [height](widgets/Positioned/height.html) properties can be used to give the dimensions, with one corresponding position property (e.g. [top](widgets/Positioned/top.html) and [height](widgets/Positioned/height.html)).
+
+If all three values on a particular axis are null, then the [Stack.alignment](widgets/Stack/alignment.html) property is used to position the child.
+
+If all six values are null, the child is a non-positioned child. The [Stack](widgets/Stack-class.html) uses only the non-positioned children to size itself.
+''',
+      arguments: [
+        widthArg(),
+        heightArg(),
+        leftArg(),
+        topArg(),
+        rightArg(),
+        bottomArg(),
+      ],
+      properties: [],
+    ),
     example: '''
-TODO
+<safeArea>
+  <stack>
+    <container height="400" width="400" color="#457FDA">
+    </container>
+    <positioned left="0" top="0">
+      <container height="50" width="50" color="#7BDA45">
+      </container>
+    </positioned>
+    <positioned left="100" top="100">
+      <container height="50" width="50" color="#DA9745">
+      </container>
+    </positioned>
+    <positioned right="150" top="150" width="50" height="50">
+      <container color="#7BDA45">
+      </container>
+    </positioned>
+  </stack>
+</safeArea>
 ''',
     builder: (BuildContext context, md.Element element, RichRenderer richRenderer) async {
       final PositionedArguments arguments = PositionedArguments.fromJson(element.attributes);
