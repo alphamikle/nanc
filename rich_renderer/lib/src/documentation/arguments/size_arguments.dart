5747c4e6d9d1c85e224182c5fa9906f6274f361d
alphamikle <xelioys@gmail.com>
2023-01-07 14:11:50 -0300

    placeholder documentation

--- rich_renderer/lib/src/documentation/arguments/size_arguments.dart
+++ rich_renderer/lib/src/documentation/arguments/size_arguments.dart
@@ -4,4 +4,5 @@ import 'package:rich_renderer/src/tag_description.dart';
 TagArgument widthArg([String name = 'width']) => doubleArg(name);
 TagArgument heightArg([String name = 'height']) => doubleArg(name);
 TagArgument sizeArg([String name = 'size']) => doubleArg(name);
+TagArgument strokeWidthArg([String name = 'strokeWidth']) => doubleArg(name);
 TagArgument flexArg([String name = 'flex']) => intArg(name);
--- rich_renderer/lib/src/renderers/placeholder/placeholder_renderer.dart
+++ rich_renderer/lib/src/renderers/placeholder/placeholder_renderer.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:icons/icons.dart';
 import 'package:markdown/markdown.dart' as md;
+import 'package:rich_renderer/rich_renderer.dart';
+import 'package:rich_renderer/src/documentation/arguments/decoration_arguments.dart';
+import 'package:rich_renderer/src/documentation/arguments/scalar_arguments.dart';
+import 'package:rich_renderer/src/documentation/arguments/size_arguments.dart';
 import 'package:rich_renderer/src/renderers/placeholder/placeholder_arguments.dart';
 import 'package:rich_renderer/src/rich_renderer.dart';
 import 'package:rich_renderer/src/tag_renderer.dart';
@@ -11,37 +15,41 @@ TagRenderer placeholderRenderer() {
     tag: 'placeholder',
     pattern: RegExp(r'<placeholder.*/>'),
     endPattern: null,
+    description: TagDescription(
+      description: '''
+# [Placeholder](https://api.flutter.dev/flutter/widgets/Placeholder-class.html)
+
+A widget that draws a box that represents where other widgets will one day be added.
+
+This widget is useful during development to indicate that the interface is not yet complete.
+
+By default, the placeholder is sized to fit its container. If the placeholder is in an unbounded space, it will size itself according to the given [fallbackWidth](widgets/Placeholder/fallbackWidth.html) and [fallbackHeight](widgets/Placeholder/fallbackHeight.html).
+''',
+      arguments: [
+        colorArg(),
+        heightArg(),
+        widthArg(),
+        strokeWidthArg(),
+        stringArg('text'),
+      ],
+      properties: [],
+    ),
     example: '''
-<description>
-Placeholder
-
-A widget that draws a box that represents where other widgets will one day be added
-
-This widget is useful during development to indicate that the interface is not yet complete
-
-By default, the placeholder is sized to fit its container. If the placeholder is in an unbounded space,
-it will size itself according to the given fallbackWidth and fallbackHeight.
-
-https://api.flutter.dev/flutter/widgets/Placeholder-class.html
-</description>    
-
-<placeholder color="#FF28C018" height="50" width="100" strokeWidth="3" text="I'm groot"/>
-<placeholder color="#FF28C018" height="100" width="300" strokeWidth="1"/>
+<safeArea>
+  <center>
+    <placeholder/>
+  </center>
+</safeArea>
 ''',
     builder: (BuildContext context, md.Element element, RichRenderer richRenderer) async {
       final PlaceholderArguments arguments = PlaceholderArguments.fromJson(element.attributes);
 
       return Placeholder(
         color: arguments.color ?? Colors.black,
-        fallbackHeight: arguments.height ?? MediaQuery.of(context).size.height / 2,
-        fallbackWidth: arguments.width ?? MediaQuery.of(context).size.width,
+        fallbackHeight: arguments.height ?? 400,
+        fallbackWidth: arguments.width ?? 400,
         strokeWidth: arguments.strokeWidth ?? 2,
-        child: arguments.hasText
-            ? Padding(
-                padding: const EdgeInsets.all(8),
-                child: Text(arguments.text!),
-              )
-            : null,
+        child: arguments.hasText ? Center(child: Text(arguments.text!)) : null,
       );
     },
   );
