6e6f3758a06c810838425ce4b20942443724e921
alphamikle <xelioys@gmail.com>
2023-01-11 21:23:14 -0300

    fixes of clipboard

--- tools/lib/src/tools/clipboard.dart
+++ tools/lib/src/tools/clipboard.dart
@@ -1,10 +1,12 @@
-import 'package:super_clipboard/super_clipboard.dart';
+import 'package:flutter/services.dart';
+// import 'package:super_clipboard/super_clipboard.dart';
 
 Future<bool> copyTextToClipboard(String? value) async {
   if (value != null) {
-    await ClipboardWriter.instance.write([
-      DataWriterItem()..add(Formats.plainText(value)),
-    ]);
+    await Clipboard.setData(ClipboardData(text: value));
+    // await ClipboardWriter.instance.write([
+    //   DataWriterItem()..add(Formats.plainText(value)),
+    // ]);
     return true;
   }
   return false;
